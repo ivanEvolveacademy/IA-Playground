6ad98c07d5fc6ef085dce5208174dab9ef834807
ivan <ivansl@edu.evolveacademy.es>
2025-07-15 19:48:31 +0200

    fix: correct required field declaration for phone number in User schema

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   age: { type: Number, required: true },
   dni: { type: String, required: true }, // Added field for DNI
-  tlf: { type: String, required: true } // Added field for phone number
+  tlf: { type: String, required: t // Added field for phone number
 });
+
 
 module.exports = mongoose.model('User', userSchema);
